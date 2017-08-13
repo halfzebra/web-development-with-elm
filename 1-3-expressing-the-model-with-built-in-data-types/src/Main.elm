829216b613eb16301496a40f70b8f1b1e097bcaf
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2017-08-13 17:59:49 +0200

    Added examples of working with Tuples and Records

--- 1-3-expressing-the-model-with-built-in-data-types/src/Main.elm
+++ 1-3-expressing-the-model-with-built-in-data-types/src/Main.elm
@@ -3,5 +3,57 @@ module Main exposing (..)
 import Html exposing (text)
 
 
+-- Using Tuples
+
+
+questionTuple =
+    ( "Why did the chicken cross the road?", "To get to the other side" )
+
+
+anotherQuestionTuple =
+    ( Tuple.first questionTuple, "It was too far to walk around" )
+
+
+questionTupleView ( question, answer ) =
+    text ("Question: " ++ question ++ " Answer: " ++ answer)
+
+
+tupleWithDfifferentTypes x str =
+    ( x, str )
+
+
+(=>) =
+    (,)
+
+
+
+-- Using Records
+
+
+questionRecord =
+    { question = "Why did the chicken cross the road?"
+    , answer = "To get to the other side"
+    }
+
+
+anotherQuestionRecord =
+    { questionRecord | answer = "It was too far to walk around" }
+
+
+questionRecordView { question, answer } =
+    text ("Question: " ++ question ++ " Answer: " ++ answer)
+
+
+
+{- Alternative implementation without destructuring:
+
+   questionRecordView model =
+       text ("Question: " ++ model.question ++ " Answer: " ++ model.answer)
+       --    text ("Question: " ++ (.question model) ++ " Answer: " ++ (.answer model))
+
+-}
+
+
 main =
-    text "Hello!"
+    -- questionRecordView questionRecord
+    questionTupleView questionTuple
