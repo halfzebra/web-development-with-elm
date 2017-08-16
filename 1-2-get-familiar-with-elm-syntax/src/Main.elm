01b4f586794185a89425ac6e03a449fa98434732
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2017-08-16 18:44:04 +0200

    Update the 1-2 example

--- 1-2-get-familiar-with-elm-syntax/src/Main.elm
+++ 1-2-get-familiar-with-elm-syntax/src/Main.elm
@@ -3,50 +3,27 @@ module Main exposing (..)
 import Html exposing (text)
 
 
-message =
-    "Hello from Elm!"
+question =
+    "The board game Go has more possible legal positions than the number of atoms in the visible universe."
 
 
-messageFrom from =
-    "Hello from " ++ from
+answer =
+    True
 
 
-messageWithGreetingFrom greeting from =
-    greeting ++ " from " ++ from
+addLabels question answer =
+    "Question: " ++ question ++ " Answer: " ++ (toString answer)
 
 
-helloFrom =
-    messageWithGreetingFrom "Hello"
+view model =
+    model
+        |> String.toUpper
+        |> text
 
 
-replace needle replaceWith haystack =
-    String.join replaceWith (String.split needle haystack)
-
-
-
-{- Alternative implemenation using pipes:
-
-   replace needle replaceWith haystack =
-       haystack
-           |> String.split needle
-           |> String.join replaceWith
--}
-
-
-increment x =
-    x + 1
-
-
-lambdaIncrement =
-    (\x -> x + 1)
+init =
+    addLabels question answer
 
 
 main =
-    -- text "Hello"
-    -- text (message)
-    -- text (messageFrom "Elm")
-    -- text (helloFrom "Elm")
-    -- text (replace "Hello" "Hola" message)
-    message
-        |> replace "Hello" "Hola"
-        |> text
+    view init
