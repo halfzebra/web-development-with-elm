cc203e2d4a20403f47486bae4f0a19919910553d
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2017-08-20 00:22:04 +0200

    Removing unused stuff

--- 2-1-introducing-side-effects-with-beginner-program/src/Request/Helpers.elm
+++
@@ -1,9 +0,0 @@
-module Request.Helpers exposing (queryString)
-
-
-queryString : List ( String, String ) -> String
-queryString list =
-    list
-        |> List.map (\( a, b ) -> a ++ "=" ++ b)
-        |> String.join "&"
-        |> (++) "?"
