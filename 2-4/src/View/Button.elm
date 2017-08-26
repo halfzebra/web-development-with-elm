a94a3a9c9737aabe4df13eca8491d416689934b7
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2017-08-26 15:53:18 +0200

    Clean-up in 2-4

--- 2-4/src/View/Button.elm
+++ 2-4/src/View/Button.elm
@@ -1,10 +1,14 @@
 module View.Button exposing (btn)
 
-import Html exposing (Html, text, button)
+import Html exposing (Html, button, text)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 
 
 btn : msg -> String -> Html msg
 btn msg txt =
-    button [ onClick msg, class "btn btn-default" ] [ text txt ]
+    button
+        [ class "btn btn-default"
+        , onClick msg
+        ]
+        [ text txt ]
