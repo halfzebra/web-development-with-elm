8a76285a27e565e0e53b8b5eb295a21ca1314aa2
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2017-08-27 22:28:35 +0200

    Simplify 3-3

--- 3-3-testing-your-modules/tests/Decoders.elm
+++ 3-3-testing-your-modules/tests/Decoders.elm
@@ -9,63 +9,35 @@ import Request.TriviaQuestions exposing (TriviaResult)
 
 suite : Test
 suite =
-    describe "Decoders for retrieviong Trivia Questions"
-        [ test "Data.Question.decoder can decode a question with HTML special characters"
-            (\_ ->
-                let
-                    input =
-                        """
-                        {
-                           "category":"Entertainment: Japanese Anime & Manga",
-                           "type":"boolean",
-                           "difficulty":"easy",
-                           "question":"Studio Ghibli is a Japanese animation studio responsible for the films Wolf Children and The Boy and the Beast.",
-                           "correct_answer":"False",
-                           "incorrect_answers":[
-                              "True"
-                           ]
-                        }
-                        """
+    test "Request.TriviaQuestions.decoder can decode correct JSON"
+        (\_ ->
+            let
+                input =
+                    """
+                    {
+                       "response_code": 0,
+                       "results":[{
+                       "question": "Testting decoders is quite easy",
+                       "correct_answer":"True",
+                        "incorrect_answers":[
+                           "False"
+                        ]}]
+                    }
+                    """
 
-                    decodedOutput =
-                        Json.Decode.decodeString Data.Question.decoder input
-                in
-                    Expect.equal decodedOutput
-                        (Ok
-                            (Question
+                decodedOutput =
+                    Json.Decode.decodeString Request.TriviaQuestions.decoder input
+            in
+                Expect.equal
+                    decodedOutput
+                    (Ok
+                        (TriviaResult 0
+                            [ Question
                                 Nothing
-                                "Studio Ghibli is a Japanese animation studio responsible for the films Wolf Children and The Boy and the Beast."
-                                "False"
-                                [ "True" ]
-                            )
+                                "Testting decoders is quite easy"
+                                "True"
+                                [ "False" ]
+                            ]
                         )
-            )
-        , test "Request.TriviaQuestions.decoder can decode correct JSON"
-            (\_ ->
-                let
-                    input =
-                        """
-                        {
-                           "response_code": 0,
-                           "results":[{
-                           "question": "Testting decoders is quite easy",
-                           "correct_answer":"True",
-                            "incorrect_answers":[
-                               "False"
-                            ]}]
-                        }
-                        """
-
-                    decodedOutput =
-                        Json.Decode.decodeString Request.TriviaQuestions.decoder input
-                in
-                    Expect.equal
-                        decodedOutput
-                        (Ok
-                            (TriviaResult
-                                0
-                                [ (Question Nothing "Testting decoders is quite easy" "True" [ "False" ]) ]
-                            )
-                        )
-            )
-        ]
+                    )
+        )
